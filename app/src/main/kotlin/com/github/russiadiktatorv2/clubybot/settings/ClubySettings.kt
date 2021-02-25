105fdf4072fbff17bff4b69744975f9ee99ee069
RussiaDiktator <timur_13@web.de>
2021-02-25 18:17:28 +0100

    Changed Database and the Prefix of the bot.

--- app/src/main/kotlin/com/github/russiadiktatorv2/clubybot/settings/ClubySettings.kt
+++ app/src/main/kotlin/com/github/russiadiktatorv2/clubybot/settings/ClubySettings.kt
@@ -2,7 +2,7 @@ package com.github.russiadiktatorv2.clubybot.settings
 
 object ClubySettings {
 
-    const val BOT_TOKEN = "ODA2NTY2MTM1ODAxNTc3NTEz.YBrTQA.RWnKjxL_OSA_XMaD8vRoe84prUs"
+    const val BOT_TOKEN = ""
     const val BOT_PREFIX = "!"
 
 }
